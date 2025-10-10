d6e7756187b29ee50249f005cf192a8a86de4c4e
Pablo Barreto <pablojbarreto@gmail.com>
2025-10-10 20:43:54 -0300

    Modificaciones al Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,69 @@
 import React from 'react';
-import {Container, Row} from "react-bootstrap"
+import {Container, Row, Col, Stack, Image} from "react-bootstrap"
+import react from "../images/react.svg";
+import vite from "../images/vite.svg";
+import bootstrap from "../images/bootstrap.svg";
+import pokeapi from "../images/pokeapi.png";
+
 function Footer() {
   return (
-      <footer>
+      <footer >
           <Container fluid>
-              <Row className='bg-dark text-white'>
-      <p>&copy; Creado por Pablo Barreto | 2025
-                      | Todos los derechos reservados</p>
-                  </Row>
+        <Row className='bg-dark text-white p-2'>
+          
+          <Col className='d-flex justify-content-center'>
+            
+            <div className="mx-1">
+              <Image
+                src={react} alt='react logo'
+                width={20}
+                height={20}
+                className="img-fluid"               
+              /></div>
+            <div className="mx-1">
+              <Image
+                src={vite} alt='vite logo'
+                width={20}
+                height={20}
+                 className="img-fluid"               
+              /></div>
+            <div className="mx-1">
+              <Image
+                src={bootstrap} alt='bootstrap logo'
+                width={20}
+                height={20}
+                      className="img-fluid"               
+              />
+            </div>
+            <div className="d-flex flex-column align-items-center">
+            <p className="mx-1">Desarrollado con React + Vite + Bootstrap</p>  
+              </div>
+          
+          </Col>
+          <Col>
+        <p>&copy; Creado por Pablo Barreto | 2025</p>
+                      
+          </Col>
+          <Col className='d-flex justify-content-center'>
+        <div className="mx-1">
+              <Image
+                src={pokeapi} alt='pokeapi logo'
+                width={50}
+                height={50}
+                className="img-fluid"               
+              /></div>
+            <div className="d-flex flex-column align-items-center">
+            <p className="mx-1">Datos oficiales de PokeAPI</p>  
+              </div>
+            
+                      
+          </Col>
+        {/* </Row>
+        <Row className='bg-dark text-white'>
+      <p>Desarrollado con React + Vite + Bootstrap| Datos oficiales de PokeAPI</p>
+                  
+ */}
+          </Row>
     </Container>
       </footer>
   );
